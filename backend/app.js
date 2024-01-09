c0ac6d031b0c30a1c558c2391fba7ec06513a764
sefedemircan <sefedemircan@gmail.com>
2024-01-09 12:27:09 +0300

    ss

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,15 @@ var usersRouter = require('./routes/users');
 var apiRouter= require("./app_api/routes/index"); 
 var app = express();
 app.use(cors());
+var allowCrossDomain = function(req, res, next) { 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    }
+    app.use(allowCrossDomain);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api",apiRouter);
