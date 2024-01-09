5eb332774594f99c7cf9a2bfdfb722f23baaf607
Sadık Efecan Demircan <97389775+sefedemircan@users.noreply.github.com>
2024-01-09 11:48:31 +0300

    update backend link

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://mekanbul-sefe.vercel.app/api",
+  baseURL: "https://mekanbul-sefe-backend.vercel.app/api",
   headers: {
     "Accept": "application/json",
     "Content-Type": "application/json;charset=UTF-8"
